ef59154de3f11e3825239679e6d358dac0852beb
joshdrummond <joshdrummond@8fd7b1e5-901e-0410-8b18-53dfffbac92a>
2007-01-03 03:35:06 +0000

    Updates to documentation and licensing

--- trunk/src/com/joshdrummond/liverecordingreview/controller/AddRecordingController.java
+++ trunk/src/com/joshdrummond/liverecordingreview/controller/AddRecordingController.java
@@ -1,6 +1,23 @@
-/**
- * 
- */
+/*
+    Copyright 2006, 2007 Josh Drummond
+
+    This file is part of LiveRecordingReview.
+
+    LiveRecordingReview is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    LiveRecordingReview is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 package com.joshdrummond.liverecordingreview.controller;
 
 import java.util.HashMap;
@@ -9,13 +26,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
-
 import com.joshdrummond.liverecordingreview.model.Category;
 import com.joshdrummond.liverecordingreview.model.Recording;
 import com.joshdrummond.liverecordingreview.service.RecordingService;
 
+
 /**
- * @author josh
+ * Spring MVC Form Controller for adding a new recording to a given artist/category
+ * 
+ * @author Josh Drummond
  *
  */
 public class AddRecordingController extends SimpleFormController
@@ -29,6 +48,7 @@ public class AddRecordingController extends SimpleFormController
         return new ModelAndView(getSuccessView(), "id", recording.getCategory().getId());
     }
     
+    
     private RecordingService recordingService;
     public void setRecordingService(RecordingService recordingService)
     {
@@ -36,7 +56,9 @@ public class AddRecordingController extends SimpleFormController
     }
   
 
-    protected Object formBackingObject(HttpServletRequest request) throws Exception {
+    protected Object formBackingObject(HttpServletRequest request)
+        throws Exception
+    {
         Category category = new Category();
         Recording recording = new Recording();
         category.setId(Integer.parseInt(request.getParameter("id")));
@@ -48,8 +70,9 @@ public class AddRecordingController extends SimpleFormController
     }
 
     
-    protected Map referenceData(HttpServletRequest request) throws Exception {
-        
+    protected Map referenceData(HttpServletRequest request)
+        throws Exception
+    {
         Map<Object, Object> reference = new HashMap<Object, Object>();
 
         // generate typeCode & description list
